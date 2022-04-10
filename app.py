520fd3c217d00af9bbce451a63dd40ce126d2f97
Abhishek <abhishukla2012@gmail.com>
2022-04-10 12:19:46 +0530

    first commit

--- app.py
+++ app.py
@@ -79,5 +79,9 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True)
+    app.run(host='127.0.0.1', port=8001, debug=True)
+	#app.run(debug=True)
+
+
+#deply on heroku https://devcenter.heroku.com/articles/heroku-cli
+#https://dashboard.heroku.com/apps
